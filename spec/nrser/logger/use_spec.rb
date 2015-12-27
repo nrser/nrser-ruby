39d84cab62aa4ec0d4393ad0ac7598217d2a221b
nrser <neil@ztkae.com>
2015-12-27 00:12:16 -0800

    NRSER::Logger.use spec

---
+++ spec/nrser/logger/use_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'NRSER::Logger.use' do
+  it "points to the source logger" do
+    module Source
+      NRSER::Logger.install self, on: true, say_hi: false
+      
+      module Target
+        NRSER::Logger.use Source, self
+      end
+    end
+    
+    expect_to_log { Source::Target.info "here" }
+    expect(Source::Target.logger).to be Source.logger
+  end
+end
