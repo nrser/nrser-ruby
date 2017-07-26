42c63032c30fb93f254163b8679699842c7de15f
nrser <neil@ztkae.com>
2017-07-26 20:19:47 +0800

    doc for NRSER.as_hash

--- lib/nrser/hash.rb
+++ lib/nrser/hash.rb
@@ -20,7 +20,7 @@ module NRSER
   # Treat the value as the value for `key` in a hash if it's not already a
   # hash and can't be converted to one:
   # 
-  # 1.  If the value is a {Hash}, return it.
+  # 1.  If the value is a `Hash`, return it.
   # 2.  If the value responds to `#to_h` and `#to_h` succeeds, return the
   #     resulting hash.
   # 3.  Otherwise, return a new hash where `key` points to the value.
@@ -30,8 +30,59 @@ module NRSER
   # to be called with a single value that corresponds to a default key in that
   # option hash.
   # 
-  # @example
-  #   Say you have a 
+  # Refinement
+  # ----------
+  # 
+  # Added to `Object` in `nrser/refinements`.
+  # 
+  # 
+  # Example Time!
+  # -------------
+  # 
+  # Say you have a method `m` that handles a hash of HTML options that can
+  # look something like
+  # 
+  #     {class: 'address', data: {confirm: 'Really?'}}
+  # 
+  # And can call `m` like
+  # 
+  #     m({class: 'address', data: {confirm: 'Really?'}})
+  # 
+  # but often you are just dealing with the `:class` option. You can use
+  # {NRSER.as_hash} to accept a string and treat it as the `:class` key:
+  # 
+  #     using NRSER
+  #     
+  #     def m opts
+  #       opts = opts.as_hash :class
+  #       # ...
+  #     end
+  # 
+  # If you pass a hash, everything works normally, but if you pass a string
+  # `'address'` it will be converted to `{class: 'address'}`.
+  # 
+  # 
+  # About `#to_h` Support
+  # ---------------------
+  # 
+  # Right now, {.as_hash} also tests if `value` responds to `#to_h`, and will
+  # try to call it, using the result if it doesn't raise. This lets it deal 
+  # with Ruby's "I used to be a Hash until someone mapped me" values like
+  # `[[:class, 'address']]`. I'm not sure if this is the best approach, but 
+  # I'm going to try it for now and see how it pans out in actual usage.
+  # 
+  # @todo
+  #   It might be nice to have a `check` option that ensures the resulting
+  #   hash has a value for `key`.
+  # 
+  # @param [Object] value
+  #   The value that we want to be a hash.
+  # 
+  # @param [Object] key
+  #   The key that `value` will be stored under in the result if `value` is 
+  #   not a hash or can't be turned into one via `#to_h`.
+  # 
+  # @return [Hash]
   # 
   def self.as_hash value, key
     return value if value.is_a? Hash
