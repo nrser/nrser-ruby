1a0bb7f6b410b59fbb172306afbcb055dfadeee6
nrser <neil@ztkae.com>
2017-08-20 00:56:55 +0800

    Ported class_attr_accessor over and added tests

---
+++ lib/nrser/class_attrs.rb
@@ -0,0 +1,74 @@
+module NRSER 
+  
+  # Mixin to provide methods to define and access class attributes - variables
+  # that act like instance variables with regards to inheritance but for the
+  # class itself.
+  # 
+  # The motivation is to create a easy-to-use class instance variables that 
+  # resolve like regular instance variables by looking up the inheritance
+  # hierarchy - meaning that:
+  # 
+  # 1.  When the value is set, it is set on the class in which the operation
+  #     happens.
+  #     
+  # 2.  That value is read for that class and any subclasses.
+  #     -   Class 'self' attr_accessor values are not visible to subclasses.
+  #     
+  # 3.  But that value is not visible to any classes further up the inheritance
+  #     chain.
+  #     -   Class variables (`@@` variables) are global to the *entire class
+  #         hierarchy* rooted at the definition point.
+  # 
+  # The tests in `spec/nrser/class_attrs_spec.rb` provide detailed walk-through
+  # of usage and differences from other approaches.
+  # 
+  module ClassAttrs 
+    
+    # Class methods to extend the receiver with when {NRSER::ClassAttrs}
+    # is included.
+    module ClassMethods
+      def instance_variable_lookup name
+        instance_variable_get(name) || if (
+          superclass.respond_to? :instance_variable_lookup
+        )
+          superclass.instance_variable_lookup name
+        else
+          raise NoMethodError.new NRSER.squish <<-END
+            #{ name.inspect } is not defined anywhere in the class hierarchy
+          END
+        end
+      end
+
+      def class_attr_accessor attr
+        var_name = "@#{ attr }".to_sym
+        
+        singleton_class.class_eval do
+          define_method(attr) do |*args|
+            case args.length
+            when 0
+              instance_variable_lookup var_name
+            when 1
+              instance_variable_set var_name, args[0]
+            else
+              raise ArgumentError.new NRSER.squish <<-END
+                wrong number of arguments
+                (given #{ args.length }, expected 0 or 1)
+              END
+            end
+          end
+          
+          define_method("#{ attr }=") do |value|
+            instance_variable_set var_name, value
+          end
+        end
+      end
+    end # module ClassMethods
+    
+    # Extend the including class with
+    # {QB::Util::ClassInstanceMethods::ClassMethods}
+    def self.included base
+      base.extend ClassMethods
+    end
+    
+  end # module ClassAttrs
+end # module NRSER
---
+++ spec/nrser/class_attrs_spec.rb
@@ -0,0 +1,241 @@
+require 'spec_helper'
+
+describe NRSER::ClassAttrs do
+  # I'm writing this as I'm developing the module, so I'm going to walk through
+  # some of the motivation, features and differences from other approaches
+  # as notes to self/others...
+  # 
+  
+  
+  # Setup
+  # =====================================================================
+  # 
+  # Because any changes would global and RSpec seems to do things some-what
+  # asynchronously we need to use dynamic class creation, which is sub-optimal
+  # because it has subtle difference with normal definitions (@@ variable
+  # declaration being the major one I've hit do far), but it seems like the 
+  # most reasonable way to go about it at the moment.
+  # 
+  
+  let (:base) {
+    Class.new do
+      include NRSER::ClassAttrs
+      
+      # To compare, I'll also work with standard '@@' class variables and 
+      # an standard attr_accessors added to the classes themselves.
+      
+      # Normally this would be
+      # 
+      # @@base_class_var = :base_class_var_value
+      # 
+      # but that is different for dynamic classes, to we use
+      class_variable_set :@@base_class_var, :base_class_var_value
+      
+      class << self
+        attr_accessor :base_self_attr_accessor
+      end
+      
+      # Set the self attr_accessor value
+      self.base_self_attr_accessor = :base_self_attr_accessor_value
+      
+      # And now some variables using the mixin.
+      
+      # I won't ever set this
+      class_attr_accessor :never_set
+      
+      # I'll define this here, but won't set it from base 
+      class_attr_accessor :def_in_base
+      
+      # These will be set from here
+      class_attr_accessor :set_in_base_1
+      class_attr_accessor :set_in_base_2
+      
+      # There are two ways of setting values:
+      # 
+      # 1.  self assignment:
+      
+      self.set_in_base_1 = :set_in_base_1_value
+      
+      #     Not that `x = value` will not work - the `self` is required.
+      #     I haven't found any way to get `x = value` to trigger the setter
+      #     methods.
+      #     
+      # 2.  "DSL-style":
+      
+      set_in_base_2 :set_in_base_2_value
+      
+      #     This is the style i started with, because it made it really easy
+      #     to have nice-feeling DSL-like functionality with minimal work.
+      #     
+      #     I'm unsure at the moment if it's something I want to keep (probably),
+      #     and if so if it should be optional via a flag on
+      #     `class_attr_accessor` (seems like a good idea).
+      #     
+      #     It's "nice", but it's perhaps kind-of counter-intuitive in the Ruby
+      #     world?
+      # 
+      
+    end
+  }
+  
+  let(:child_1) {
+    Class.new(base) do
+    end
+  }
+  
+  let(:child_1_1) {
+    Class.new(child_1) do
+    end
+  }
+  
+  let(:child_2) {  
+    Class.new(base) do
+    end
+  }
+  
+  let(:child_2_1) {
+    Class.new(child_2) do
+    end
+  }
+  
+  
+  # Tests
+  # =====================================================================
+  
+  describe "Class (@@) Variables and Their Problems" do
+  # ---------------------------------------------------------------------
+  # 
+  # Examples of the desired features that work (OK examples) and those that
+  # don't (PROBLEM! examples) for class (@@) variables.
+  # 
+  
+    it "can access @@ variables from base and any subclass (OK)" do    
+      [base, child_1, child_1_1, child_2, child_2_1].each do |klass|
+        expect(
+          klass.class_variable_get :@@base_class_var
+        ).to be :base_class_var_value
+      end
+    end
+    
+    
+    context "@@ variable changed in base" do
+      let(:new_value) { :new_base_class_var_value }
+      
+      before {
+        base.class_variable_set :@@base_class_var, new_value
+      }
+      
+      it "can access new value from base and any subclass (OK)" do    
+        [base, child_1, child_1_1, child_2, child_2_1].each do |klass|
+          expect(
+            klass.class_variable_get :@@base_class_var
+          ).to be new_value
+        end
+      end
+    end # @@ variable changed in base
+    
+    
+    context "@@ variable changed in subclass" do
+      let(:new_value) { :new_child_1_class_var_value }
+      
+      before {
+        child_1.class_variable_set :@@base_class_var, new_value
+      }
+      
+      # This is the problem: changes to the value anywhere in the hierarchy 
+      # are global to the hierarchy 
+      it "reads that value from all classes (PROBLEM!)" do    
+        [base, child_1, child_1_1, child_2, child_2_1].each do |klass|
+          expect(
+            klass.class_variable_get :@@base_class_var
+          ).to be new_value
+        end
+      end
+    end # @@ variable changed in base
+    
+  end # Class (@@) Variables and Their Problems
+  
+  
+  describe "Class 'Self' attr_accessor and Their Problems" do
+  # ---------------------------------------------------------------------
+    
+    it "can access from base (OK)" do
+      expect(base.base_self_attr_accessor).to be :base_self_attr_accessor_value
+    end
+    
+    it "can't access from any subclasses (PROBLEM!)" do
+      [child_1, child_1_1, child_2, child_2_1].each do |klass|
+        expect(klass.base_self_attr_accessor).to be nil
+      end
+    end
+    
+  end # Class 'Self' Attribute Accessors and Their Problems
+  
+  
+  
+  describe "NRSER::ClassAttrs Solution" do
+  # ---------------------------------------------------------------------
+  # 
+  # Examples of the desired features that work (OK examples) and those that
+  # don't (PROBLEM! examples) for class 'self' attr_accessor.
+  # 
+  
+    it "raises NoMethodError when accessing unset class attrs" do
+      expect { base.never_set }.to raise_error NoMethodError
+      expect { base.def_in_base }.to raise_error NoMethodError
+    end
+    
+    
+    it "reads values set in base from base and any subclass" do
+      [base, child_1, child_1_1, child_2, child_2_1].each do |klass|
+        expect(klass.set_in_base_1).to be :set_in_base_1_value
+        expect(klass.set_in_base_2).to be :set_in_base_2_value
+      end
+    end
+    
+    
+    context "class_attr_accessor value changed in base" do
+      # With class_attr_accessor, the new value will be visible to base and
+      # all subclasses.
+      
+      let(:new_value) { :new_set_in_base_1_value }
+      
+      before {
+        base.set_in_base_1 = new_value
+      }
+      
+      it "can access new value from base and any subclass" do    
+        [base, child_1, child_1_1, child_2, child_2_1].each do |klass|
+          expect(klass.set_in_base_1).to be new_value
+        end
+      end
+    end # class_attr_accessor value changed in base
+    
+    
+    context "class_attr_accessor value changed in subclass" do
+      # With class_attr_accessor, the new value will be visible to the class
+      # it is set in and any subclasses without affecting any others.
+      
+      let(:new_value) { :set_in_child_1 }
+      
+      before {
+        child_1.set_in_base_1 = new_value
+      }
+      
+      it "reads the new value from that class and any subclasses" do    
+        [child_1, child_1_1].each do |klass|
+          expect(klass.set_in_base_1).to be new_value
+        end
+      end
+      
+      it "reads the old value from any other classes" do    
+        [base, child_2, child_2_1].each do |klass|
+          expect(klass.set_in_base_1).to be :set_in_base_1_value
+        end
+      end
+      
+    end # class_attr_accessor value changed in subclass
+    
+  end # NRSER::ClassAttrs Solution
+  
+end # NRSER::ClassAttrs
