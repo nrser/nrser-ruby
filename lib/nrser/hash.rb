071df6d7cbe9c0679b90e169c7636c00ac3bd766
nrser <neil@ztkae.com>
2017-08-20 21:11:16 +0800

    Add stringify_keys methods.

--- lib/nrser/hash.rb
+++ lib/nrser/hash.rb
@@ -238,21 +238,58 @@ module NRSER
   # @see http://www.rubydoc.info/gems/activesupport/5.1.3/Hash:symbolize_keys!
   # 
   # @param [Hash] hash
-  #   Hash to mutate.
   # 
-  # @return [return_type]
-  #   @todo Document return value.
+  # @return [Hash]
   # 
   def self.symbolize_keys! hash
     transform_keys!(hash) { |key| key.to_sym rescue key }
   end # .symbolize_keys!
   
   
-  
+  # Returns a new hash with all keys that respond to `#to_sym` converted to
+  # symbols.
+  # 
+  # Lifted from ActiveSupport.
+  # 
+  # @see http://www.rubydoc.info/gems/activesupport/5.1.3/Hash:symbolize_keys
+  # 
+  # @param [Hash] hash
+  # 
+  # @return [Hash]
+  # 
   def self.symbolize_keys hash
     # File 'lib/active_support/core_ext/hash/keys.rb', line 54
     transform_keys(hash) { |key| key.to_sym rescue key }
   end
+  
+  
+  # Converts all keys into strings by calling `#to_s` on them. **Mutates the
+  # hash.**
+  # 
+  # Lifted from ActiveSupport.
+  # 
+  # @param [Hash] hash
+  # 
+  # @return [Hash<String, *>]
+  # 
+  def self.stringify_keys! hash
+    transform_keys! hash, &:to_s
+  end
+  
+  
+  # Returns a new hash with all keys transformed to strings by calling `#to_s`
+  # on them.
+  # 
+  # Lifted from ActiveSupport.
+  # 
+  # @param [Hash] hash
+  # 
+  # @return [Hash<String, *>]
+  # 
+  def self.stringify_keys hash
+    transform_keys hash, &:to_s
+  end
+  
   
   # Lifted from ActiveSupport.
   # 
--- lib/nrser/refinements/hash.rb
+++ lib/nrser/refinements/hash.rb
@@ -33,29 +33,44 @@ module NRSER
       NRSER.map_hash_values self, &block
     end
     
-    
+    # See {NRSER.transform_keys!}
     def transform_keys! &block
       return enum_for(:transform_keys!) { size } unless block_given?
       NRSER.transform_keys! self, &block
     end
     
     
+    # See {NRSER.transform_keys}
     def transform_keys &block
       return hash.enum_for(:transform_keys) { size } unless block_given?
       NRSER.transform_keys self, &block
     end
     
     
+    # See {NRSER.symbolize_keys!}
     def symbolize_keys!
       NRSER.symbolize_keys! self
     end
     
     
+    # See {NRSER.symbolize_keys}
     def symbolize_keys
       NRSER.symbolize_keys self
     end
     
     
+    # See {NRSER.stringify_keys!}
+    def stringify_keys!
+      NRSER.stringify_keys! self
+    end
+    
+    
+    # See {NRSER.stringify_keys}
+    def stringify_keys
+      NRSER.stringify_keys self
+    end
+    
+    # See {NRSER.map_hash_keys}
     def map_keys &block
       NRSER.map_hash_keys self, &block
     end
