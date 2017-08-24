19002d90bb6236a74f54eed58b2c0bcc8b0b720b
nrser <neil@ztkae.com>
2017-08-24 22:37:18 +0800

    merge additional Exception refinements

--- lib/nrser/refinements/exception.rb
+++ lib/nrser/refinements/exception.rb
@@ -4,4 +4,28 @@ module NRSER
       NRSER.format_exception self
     end
   end
+  
+  # Create a new instance from the squished message.
+  # 
+  # See {NRSER.squish}.
+  # 
+  # @param [String] message
+  # 
+  # @return [Exception]
+  # 
+  def self.squished message
+    new message.squish
+  end
+  
+  # Create a new instance from the dedented message.
+  # 
+  # See {NRSER.dedent}.
+  # 
+  # @param [String] message
+  # 
+  # @return [Exception]
+  # 
+  def self.dedented message
+    new message.dedent
+  end
 end # NRSER
