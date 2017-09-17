440545e2888682ce41f26b7fd310bb6b0ac8bab6
nrser <neil@ztkae.com>
2017-09-17 16:26:38 +0800

    Add NRSER.rest and Array#rest refinement

--- lib/nrser/array.rb
+++ lib/nrser/array.rb
@@ -1,37 +1,58 @@
-module NRSER
+module NRSER  
   
-  # Return an array given any value in the way that makes most sense:
+  # Eigenclass (Singleton Class)
+  # ========================================================================
   # 
-  # 1.  If `value` is an array, return it.
-  #     
-  # 2.  If `value` is `nil`, return `[]`.
-  #     
-  # 3.  If `value` responds to `#to_a`, try calling it. If it succeeds, return
-  #     that.
-  #     
-  # 4.  Return an array with `value` as it's only item.
-  # 
-  # Refinement
-  # ----------
-  # 
-  # Added to `Object` in `nrser/refinements`.
-  # 
-  # @param [Object] value
-  # 
-  # @return [Array]
-  # 
-  def self.as_array value
-    return value if value.is_a? Array
-    return [] if value.nil?
+  class << self
     
-    if value.respond_to? :to_a
-      begin
-        return value.to_a
-      rescue
+    # Return an array given any value in the way that makes most sense:
+    # 
+    # 1.  If `value` is an array, return it.
+    #     
+    # 2.  If `value` is `nil`, return `[]`.
+    #     
+    # 3.  If `value` responds to `#to_a`, try calling it. If it succeeds, return
+    #     that.
+    #     
+    # 4.  Return an array with `value` as it's only item.
+    # 
+    # Refinement
+    # ----------
+    # 
+    # Added to `Object` in `nrser/refinements`.
+    # 
+    # @param [Object] value
+    # 
+    # @return [Array]
+    # 
+    def as_array value
+      return value if value.is_a? Array
+      return [] if value.nil?
+      
+      if value.respond_to? :to_a
+        begin
+          return value.to_a
+        rescue
+        end
       end
-    end
+      
+      [value]
+    end # #as_array
     
-    [value]
-  end # .as_array
+    
+    # Functional implementation of "rest" for arrays. Used when refining `#rest`
+    # into {Array}.
+    # 
+    # @param [Array] array
+    # 
+    # @return [return_type]
+    #   New array consisting of all elements after the first.
+    # 
+    def rest array
+      array[1..-1]
+    end # #rest
+    
+    
+  end # class << self (Eigenclass)
   
 end # module NRSER
--- lib/nrser/refinements/array.rb
+++ lib/nrser/refinements/array.rb
@@ -3,5 +3,15 @@ require_relative './enumerable'
 module NRSER
   refine ::Array do
     include NRSER::Refinements::Enumerable
+    
+    
+    # @return [Array]
+    #   new array consisting of all elements after the first (which may be 
+    #   none, resulting in an empty array).
+    # 
+    def rest
+      NRSER.rest self
+    end # #rest
+    
   end # refine ::Array
 end # NRSER
