5801262ecc0076337c1bb7d1b3329ccb2838f965
nrser <neil@ztkae.com>
2017-09-26 04:54:16 +0800

    Prop@to_data attr and allow Prop#source to be Proc

--- lib/nrser/meta/props/prop.rb
+++ lib/nrser/meta/props/prop.rb
@@ -1,8 +1,9 @@
 require 'nrser/no_arg'
 
 require 'nrser/refinements'
-require 'nrser/refinements/types'
+using NRSER
 
+require 'nrser/refinements/types'
 using NRSER::Types
 
 module NRSER 
@@ -20,13 +21,15 @@ class Prop
                   name,
                   type: t.any,
                   default: NRSER::NO_ARG,
-                  source: nil
+                  source: nil,
+                  to_data: nil
     
     @defined_in = defined_in
     @name = name
     @type = t.make type
     @source = source
     @default = default
+    @to_data = to_data
     
     if @source.nil?
       @instance_variable_source = false
@@ -113,7 +116,17 @@ class Prop
       if instance_variable_source?
         instance.instance_variable_get source
       else
-        instance.send source
+        case source
+        when String, Symbol
+          instance.send source
+        when Proc
+          instance.instance_exec &source
+        else
+          raise TypeError.squished <<-END
+            Expected `Prop#source` to be a String, Symbol or Proc;
+            found #{ source.inspect }
+          END
+        end
       end
     else
       values(instance)[name]
@@ -166,25 +179,82 @@ class Prop
   end # #set_from_hash
   
   
-  
-  # @todo Document to_data method.
+  # Get the "data" value - a basic scalar or structure of hashes, arrays and
+  # scalars, suitable for JSON encoding, etc. - for the property from an
+  # instance.
   # 
-  # @param [type] arg_name
-  #   @todo Add name param description.
+  # The process depends on the `to_data:` keyword provided at property
+  # declaration:
   # 
-  # @return [return_type]
-  #   @todo Document return value.
+  # 1.  {nil} *default*
+  #     -   If the property value responds to `#to_data`, the result of 
+  #         invoking that method will be returned.
+  #         
+  #         **WARNING**
+  #         
+  #         This can cause infinite recursion if an instance has
+  #         a property value that is also an instance of the same class (as
+  #         as other more complicated scenarios that boil down to the same 
+  #         problem), but, really, what else would it do in this situation?
+  #         
+  #         This problem can be avoided by by providing a `to_data:` keyword
+  #         when declaring the property that dictates how to handle it's value.
+  #         In fact, that was the motivation for adding `to_data:`.
+  #         
+  #     -   Otherwise, the value itself is returned, under the assumption that
+  #         it is already suitable as data.
+  #         
+  # 2.  {Symbol} | {String}
+  #     -   The `to_data:` string or symbol is sent to the property value
+  #         (the method with this name is called via {Object#send}).
+  #         
+  # 3.  {Proc}
+  #     -   The `to_data:` proc is called with the property value as the sole
+  #         argument and the result is returned as the data.
+  # 
+  # @param [NRSER::Meta::Props] instance
+  #   Instance to get the property value form.
+  # 
+  # @return [Object]
+  #   Data representation of the property value (hopefully - the value itself
+  #   is returned if we don't have any better options, see above).
+  # 
+  # @raise [TypeError]
+  #   If {@to_data} (provided via the `to_data:` keyword at property 
+  #   declaration) is anything other than {nil}, {String}, {Symbol} or {Proc}.
   # 
   def to_data instance
     value = get instance
     
-    if value.respond_to? :to_data
-      value.to_data
+    case @to_data
+    when nil
+      if value.respond_to? :to_data
+        value.to_data
+      else
+        value
+      end
+    when Symbol, String
+      value.send @to_data
+    when Proc
+      @to_data.call value
     else
-      value
+      raise TypeError.squished <<-END
+        Expected `@to_data` to be Symbol, String or Proc;
+        found #{ @to_data.inspect }
+      END
     end
   end # #to_data
   
+  
+  # @return [String]
+  #   a short string describing the instance.
+  # 
+  def to_s
+    <<-END.squish
+      #<#{ self.class.name }
+        #{ @defined_in.name }##{ @name }:#{ @type }>
+    END
+  end # #to_s  
   
   
   private
