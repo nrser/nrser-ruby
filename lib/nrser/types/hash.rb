f26b7e85254077603b34e2f07c20fb2776ae3eb0
nrser <neil@ztkae.com>
2017-10-08 18:05:24 +0800

    Some doc and org

--- lib/nrser/types/hash.rb
+++ lib/nrser/types/hash.rb
@@ -33,13 +33,33 @@ module NRSER::Types
   
   HASH = HashType.new.freeze
   
-  def self.hash_ *args
-    if args.empty?
-      HASH
-    else
-      HashType.new *args
-    end
-  end
   
-  singleton_class.send :alias_method, :dict, :hash_
+  # Eigenclass (Singleton Class)
+  # ========================================================================
+  # 
+  class << self
+    
+    # Type satisfied by {Hash} instances.
+    # 
+    # @param [Array] *args
+    #   Passed to {NRSER::Types::HashType#initialize} unless empty.
+    # 
+    # @return [NRSER::Types::HASH]
+    #   If `args` are empty.
+    # 
+    # @return [NRSER::Types::Type]
+    #   Newly constructed hash type from `args`.
+    # 
+    def hash_ *args
+      if args.empty?
+        HASH
+      else
+        HashType.new *args
+      end
+    end
+    
+    alias_method :dict, :hash_
+    
+  end # class << self (Eigenclass)
+  
 end
