02c764e623b73e49e55ee99638ec3bdd2f7f9f9c
nrser <neil@ztkae.com>
2017-11-05 04:30:16 +0800

    Rename Object#pipe refinement to Object#thru; add alias to old name.
    See comments in source.

--- lib/nrser/refinements/object.rb
+++ lib/nrser/refinements/object.rb
@@ -19,9 +19,19 @@ module NRSER
   refine Object do
     # Yield `self`. Analogous to {#tap} but returns the result of the invoked
     # block.
-    def pipe
+    def thru
       yield self
     end
+    
+    # Older name, depreciated because though 'pipe' was the natural name to me,
+    # it was probably a poor choice... it's widely used and usually denotes
+    # streaming of some sort (and rightfully so given Unix pipes).
+    # 
+    # I think I want to move over to {Object#thru}, but will leave the old
+    # name for the moment.
+    # 
+    alias_method :pipe, :thru
+    
     
     # See {NRSER.truthy?}.
     def truthy?
