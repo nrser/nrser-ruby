173fbd81746d66af342ee90f04dc8d7337f20718
nrser <neil@ztkae.com>
2017-12-31 17:33:58 +0800

    Ad NRSER::MultipleErrors, a wrapper errors around a list of others

--- lib/nrser/errors.rb
+++ lib/nrser/errors.rb
@@ -55,5 +55,52 @@ module NRSER
     end # #initialize
     
   end # class AbstractMethodError
+  
+  
+  # A wrapper error around a list of other errors.
+  # 
+  class MultipleErrors < StandardError
+    
+    # Attributes
+    # ======================================================================
+    
+    # The individual errors that occurred.
+    # 
+    # @return [Array<Exception>]
+    #     
+    attr_reader :errors
+    
+    
+    # Constructor
+    # ======================================================================
+    
+    # Instantiate a new `MultipleErrors`.
+    def initialize errors, headline: nil
+      @errors = errors
+      
+      if headline.nil?
+        class_counts = NRSER.count_by( errors, &:class ).
+          map { |klass, count| "#{ klass } (#{ count })" }.
+          join( ', ' )
+        
+        headline = "#{ errors.count } error(s) occurred - #{ class_counts }"
+      end
+      
+      message = NRSER.erb binding, <<-END
+        <%= headline %>
+        
+        <% errors.each_with_index do |error, index| %>
+        <%= (index.succ.to_s + ".").ljust( 3 ) %> <%= error.message %> (<%= error.class %>):
+            <%= error.backtrace.join( $/ ) %>
+        <% end %>
+        
+      END
+      
+      super message
+    end # #initialize
+    
+    
+  end # class MultipleErrors
+  
 
 end # module NRSER
