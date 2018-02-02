9687335a34d303a09763ae138ebed110e6ca0aa9
nrser <neil@ztkae.com>
2018-02-03 06:50:51 +0800

    Move the word split function over from QB

--- lib/nrser/functions/text.rb
+++ lib/nrser/functions/text.rb
@@ -1,3 +1,4 @@
 require_relative './text/lines'
 require_relative './text/word_wrap'
 require_relative './text/indentation'
+require_relative './text/words'
---
+++ spec/nrser/functions/text/words_spec.rb
@@ -0,0 +1,17 @@
+describe_spec_file(
+  spec_path: __FILE__,
+  module: NRSER,
+  method: :words,
+) do
+  it_behaves_like "function",
+    mapping: {
+      'some-words' => ['some', 'words'],
+      'NRSER' => ['NRSER'],
+      'NRSER::Text' => ['NRSER', 'Text'],
+      
+      # Doesn't de-camel
+      'NRSER::RSpex' => ['NRSER', 'RSpex'],
+      
+      
+    }
+end
