8bc01bc5d4a2a08e0ca55557f1b9b541eb3de09e
nrser <neil@ztkae.com>
2018-03-26 15:30:19 +0800

    Clean up / add to Source::Location spec

--- spec/lib/nrser/meta/source/location_spec.rb
+++ spec/lib/nrser/meta/source/location_spec.rb
@@ -5,49 +5,82 @@ describe_spec_file(
   class: NRSER::Meta::Source::Location,
 ) do
   
-  loc = NRSER.own_class_Methods.first.source_location
-  
-  context "instantiate from {Method#source_location}" do
-    describe_instance loc do
-      it do
-        is_expected.to be_a( NRSER::Meta::Source::Location ).
-          and have_attributes(
-            file: loc[0],
-            line: loc[1],
-            to_s: "#{ loc[0] }:#{ loc[1] }"
-          )
-      end
+  shared_examples described_class do |valid:, file:, line:|
+    it { is_expected.to be_a( described_class ) }
+    
+    describe_attribute :valid? do
+      it { is_expected.to be valid }
+    end # Attribute valid? Description
+    
+    describe_attribute :file do
+      it { is_expected.to eq file }
+    end # Attribute file Description
+    
+    describe_attribute :line do
+      it { is_expected.to eq line }
+    end # Attribute line Description
+    
+    describe_attribute :to_s do
+      it { is_expected.to eq "#{ file || '???' }:#{ line || '???' }" }
+    end # Attribute to_s Description
+    
+    describe_method :[] do
+      describe_called_with 0 do
+        it { is_expected.to eq file }
+      end # called with 0
       
-      describe_method :[] do
-        describe_called_with 0 do
-          it { is_expected.to eq loc[0] }
-        end # called with 0
-        
-        describe_called_with 1 do
-          it { is_expected.to eq loc[1] }
-        end # called with 1
-      end # Method [] Description
-      
-      describe_attribute :to_a do
-        it { is_expected.to eq loc }
-      end # Attribute to_a Description
-      
-    end # instance loc
-  end # instantiate from {Method#source_location}
+      describe_called_with 1 do
+        it { is_expected.to eq line }
+      end # called with 1
+    end # Method [] Description
+    
+    describe_attribute :to_a do
+      it { is_expected.to eq [file, line] }
+    end # Attribute to_a Description
+    
+    describe_attribute :to_h do
+      it { is_expected.to be_a Hash }
+      it { is_expected.to eq file: file, line: line }
+    end # Attribute to_h Description
+    
+  end # described_class from:
   
   
-  context "instantiate from prop value hash" do
-    describe_instance file: loc[0], line: loc[1] do
-      it do
-        is_expected.to be_a( NRSER::Meta::Source::Location ).
-          and have_attributes(
-            file: loc[0],
-            line: loc[1],
-            to_s: "#{ loc[0] }:#{ loc[1] }"
-          )
+  context "{Method#source_location} with non-nil file and line" do
+    loc = NRSER.own_class_Methods.
+      map( &:source_location ).
+      find { |src_loc|
+        !src_loc.nil? &&
+        !src_loc[0].nil? &&
+        !src_loc[1].nil?
+      }
+    
+    context "instantiate from source location array" do
+      describe_instance loc do
+        it_behaves_like described_class,
+          valid: true,
+          file: loc[0],
+          line: loc[1]
+      end # instance loc
+    end
+    
+    
+    context "instantiate from prop value hash" do
+      describe_instance file: loc[0], line: loc[1] do
+        it_behaves_like described_class,
+          valid: true,
+          file: loc[0],
+          line: loc[1]
       end
     end
-  end # instantiate from prop value hash
+  end # {Method#source_location} with non-nil file and line
+  
+  
+  context "`nil` source location" do
+    describe_instance nil do
+      it_behaves_like described_class, valid: false, file: nil, line: nil
+    end
+  end # `nil` source location
   
   
 end # spec file
