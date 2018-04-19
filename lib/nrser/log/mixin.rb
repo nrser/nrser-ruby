66d0476d08bb283a10e98c1cdb1645276bb84a9f
nrser <neil@ztkae.com>
2018-04-19 23:33:08 +0800

    Simplify {NRSER::Log::Mixin}
    Not sure *why* SemLog was using `#class_eval` instead of just module
    instance methods, but this seems to work fine and is much clearer?

--- lib/nrser/log/mixin.rb
+++ lib/nrser/log/mixin.rb
@@ -1,57 +1,56 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
-# Requirements
-# =======================================================================
-
-# Stdlib
-# -----------------------------------------------------------------------
-
-# Deps
-# -----------------------------------------------------------------------
-
-# Project / Package
-# -----------------------------------------------------------------------
-
-
-# Refinements
-# =======================================================================
-
-
-# Declarations
-# =======================================================================
-
-
 # Definitions
 # =======================================================================
 
-
-# @todo document NRSER::Log::Mixin module.
+# Adaptation of {SemanticLogger::Loggable} mixin to use {NRSER::Log::Logger}
+# instances from {NRSER::Log.[]}.
+# 
+# Like {SemanticLogger::Loggable} adds class and instance `logger` and
+# `logger=` methods that create loggers on demand and store them in the
+# `@semantic_logger` instance variables.
+# 
 module NRSER::Log::Mixin
   
-  def self.included base
-      base.extend SemanticLogger::Loggable::ClassMethods
-      base.class_eval do
-        # Returns [SemanticLogger::Logger] class level logger
-        def self.logger
-          @semantic_logger ||= NRSER::Log[self]
-        end
-
-        # Replace instance class level logger
-        def self.logger=(logger)
-          @semantic_logger = logger
-        end
-
-        # Returns [SemanticLogger::Logger] instance level logger
-        def logger
-          @semantic_logger ||= self.class.logger
-        end
-
-        # Replace instance level logger
-        def logger=(logger)
-          @semantic_logger = logger
-        end
-      end
+  # Methods to mix into the including class.
+  # 
+  module ClassMethods
+    # Returns [SemanticLogger::Logger] class level logger
+    def logger
+      @semantic_logger ||= NRSER::Log[ self ]
     end
+
+    # Replace instance class level logger
+    def logger= logger
+      @semantic_logger = logger
+    end
+  end
+  
+  
+  # Class Methods
+  # ========================================================================
+  
+  def self.included base
+    base.extend ClassMethods
+    
+    # Adds `.logger_measure_method`
+    base.extend SemanticLogger::Loggable::ClassMethods
+  end
+  
+  
+  # Instance Methods
+  # ========================================================================
+  
+  # Returns [SemanticLogger::Logger] instance level logger
+  def logger
+    @semantic_logger ||= self.class.logger
+  end
+  
+  
+  # Replace instance level logger
+  def logger= logger
+    @semantic_logger = logger
+  end
   
 end # module NRSER::Log::Mixin
