ec6377fa13c88387cc7a3da8949878e5a9956d76
nrser <neil@ztkae.com>
2018-05-06 03:41:53 +0800

    Adapt {NRSER::Log::Mixin} to deal with instance-level loggers better

--- lib/nrser/log/mixin.rb
+++ lib/nrser/log/mixin.rb
@@ -16,15 +16,26 @@ module NRSER::Log::Mixin
   # Methods to mix into the including class.
   # 
   module ClassMethods
+    
+    protected
+    # ========================================================================
+      
+      def create_logger
+        NRSER::Log[self]
+      end
+      
+    public # end protected ***************************************************
+    
     # Returns [SemanticLogger::Logger] class level logger
     def logger
-      @semantic_logger ||= NRSER::Log[ self ]
+      @semantic_logger ||= create_logger
     end
 
     # Replace instance class level logger
     def logger= logger
       @semantic_logger = logger
     end
+    
   end
   
   
@@ -42,9 +53,52 @@ module NRSER::Log::Mixin
   # Instance Methods
   # ========================================================================
   
-  # Returns [SemanticLogger::Logger] instance level logger
+  # Gets the {NRSER::Log:Logger} for use in the instance. This will be the
+  # class logger from {ClassMethods#logger} unless the instance has a
+  # `#create_logger` method.
+  # 
+  # The `#create_logger` method is expected to return a {NRSER::Log::Logger}
+  # instance, which will be saved in the `@semantic_logger` instance variable
+  # for future use.
+  # 
+  # That method does not need to return a different logger for every instance
+  # - if you just want to have a different logger for *all* instance with a
+  # different level or formatter or whatever, you can save it somewhere else
+  # and always return that same instance.
+  # 
+  # If you are dealing with frozen instances make sure to call `#logger` before
+  # you freeze (likely in the constructor). If you don't want to save the
+  # logger at all, just override this method itself.
+  # 
+  # This is a departure from {SemanticLogger::Loggable} that started because
+  # if the instance is frozen then attempting to set `@semantic_logger` will
+  # raise an error.
+  # 
+  # I also started ending up with classes that wanted to individually
+  # configure their loggers, so it seemed like we could take out two birds
+  # with this stone.
+  # 
+  # @return [SemanticLogger::Logger]
+  #   Instance level logger, if {ClassMethods.instance_logger?},
+  #   otherwise the class level logger from {ClassMethods#logger}.
+  # 
   def logger
-    @semantic_logger ||= self.class.logger
+    return @semantic_logger if @semantic_logger
+    
+    if respond_to? :create_logger, true
+      @semantic_logger = begin
+        create_logger
+      rescue Exception => error
+        self.class.logger.warn \
+          "Error creating instance logger",
+          instance: self.inspect,
+          error
+        
+        self.class.logger
+      end
+    else
+      self.class.logger
+    end
   end
   
   
