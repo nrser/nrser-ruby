d76d0efb6d3c498bdb98648beb6e80c31d4929f9
nrser <neil@ztkae.com>
2018-08-30 17:53:18 +0800

    Let describe_method take a {Method}

--- lib/nrser/rspex/example_group/describe_method.rb
+++ lib/nrser/rspex/example_group/describe_method.rb
@@ -5,44 +5,75 @@ module NRSER::RSpex::ExampleGroup
   
   # Describe a method of the parent subject.
   # 
+  # @param [Method | Symbol | String] method
+  #   The method being described:
+  #   
+  #   1.  {Method} instance - used directly.
+  #       
+  #   2.  {Symbol}, {String} - 
+  # 
   # @return [void]
   # 
-  def describe_method method_name, *description, **metadata, &body
-    # Due to legacy, we only auto-bind if the name is a symbol
-    # 
-    # TODO  Get rid of this
-    # 
-    subject_block = if method_name.is_a? Symbol
-      -> { super().method method_name }
-    end
+  def describe_method method,
+                      *description,
+                      bind_subject: nil,
+                      **metadata,
+                      &body
+    case method
+    when Method
+      method_name = method.name
+      subject_block = -> { method }
+      bind_subject = true
+      name_string = NRSER::RSpex::Format.md_code_quote \
+        "#{ method.receiver }.#{ method.name }"
     
-    name_prefix = if  self.respond_to?( :metadata ) &&
-                      self.metadata.key?( :constructor_args )
-      '#'
-    else
-      '.'
-    end
-    
-    method = if self.try( :metadata )
-      getter = if self.metadata.key?( :constructor_args )
-        :instance_method
-      else
-        :method
+    when Symbol, String
+      method_name = method
+
+      # Due to legacy, we only auto-bind if the name is a symbol
+      # 
+      # TODO  Get rid of this
+      # 
+      bind_subject = method_name.is_a?( Symbol ) if bind_subject.nil?
+
+      subject_block = if bind_subject
+        -> { super().method method_name }
       end
-      
-      target = self.metadata[:class] || self.metadata[:module]
-      
-      if target
-        begin
-          target.public_send getter, method_name
-        rescue
-          nil
+
+      name_prefix = if  self.respond_to?( :metadata ) &&
+                        self.metadata.key?( :constructor_args )
+        '#'
+      else
+        '.'
+      end
+    
+      method = if self.try( :metadata )
+        getter = if self.metadata.key?( :constructor_args )
+          :instance_method
+        else
+          :method
+        end
+        
+        target = self.metadata[:class] || self.metadata[:module]
+        
+        if target
+          begin
+            target.public_send getter, method_name
+          rescue
+            nil
+          end
         end
       end
-    end
     
-    name_string = NRSER::RSpex::Format.md_code_quote \
-      "#{ name_prefix }#{ method_name }"
+      name_string = NRSER::RSpex::Format.md_code_quote \
+        "#{ name_prefix }#{ method_name }"
+    
+    else
+      raise NRSER::TypeError.new \
+        "Expected Method, Symbol or String for `method_name`, found",
+        method_name
+      
+    end # case method_arg
     
     # Create the RSpec example group context
     describe_x \
@@ -54,6 +85,7 @@ module NRSER::RSpex::ExampleGroup
         **metadata,
         method_name: method_name,
       },
+      bind_subject: bind_subject,
       subject_block: subject_block,
       &body
   end # #describe_method
