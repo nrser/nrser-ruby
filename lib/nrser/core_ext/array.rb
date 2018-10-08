7ddd6c23f25cff6a7395f495be89edd61280a8e5
nrser <neil@neilsouza.com>
2018-10-09 02:45:08 +0800

    Move Array#to_proc to it's own file
    So it can be req'd on it's own (it's not part of core
    import at the moment)

--- lib/nrser/core_ext/array.rb
+++ lib/nrser/core_ext/array.rb
@@ -1,3 +1,5 @@
+require_relative './array/to_proc'
+
 class Array
   include NRSER::Ext::Tree
   
@@ -91,41 +93,6 @@ class Array
   def to_chainer publicly: true
     NRSER.chainer self, publicly: publicly
   end # #to_chainer
-  
-  
-  # Returns a lambda that calls accepts a single arg and calls either:
-  # 
-  # 1.  `#[self.first]` if this array has only one entry.
-  # 2.  `#dig( *self )` if this array has more than one entry.
-  # 
-  # @example
-  #   list = [{id: 1, name: "Neil"}, {id: 2, name: "Mica"}]
-  #   list.assoc_by &[:id]
-  #   # =>  {
-  #   #       1 => {id: 1, name: "Neil"},
-  #   #       2 => {id: 2, name: "Mica"},
-  #   #     }
-  # 
-  # @return [Proc]
-  #   Lambda proc that accepts a single argument and calls `#[]` or `#dig with 
-  #   this array's contents as the arguments.
-  # 
-  def to_proc
-    method_name = case count
-    when 0
-      raise NRSER::CountError.new \
-        "Can not create getter proc from empty array",
-        value: self,
-        expected: '> 0',
-        count: count
-    when 1
-      :[]
-    else
-      :dig
-    end
-      
-    NRSER::Message.new( method_name, *self ).to_proc
-  end # #to_proc
 
 
   # Old name for {#to_proc}.
