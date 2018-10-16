135a4b8662016ae6e0cd2881a87c6a4d9020d335
nrser <neil@neilsouza.com>
2018-10-17 03:40:00 +0800

    FIXME Change YARDDoc meth name for t.Enumerable
    It seems to cause bug in YARD Registry resolution,
    see not in source.

--- lib/nrser/types/enumerables.rb
+++ lib/nrser/types/enumerables.rb
@@ -92,7 +92,25 @@ end # class EnumerableType
 # @!group Enumerable Type Factories
 # --------------------------------------------------------------------------
 
-#@!method self.Enumerable **options
+# @!method self.Enumerable_ enumerable_type = ::Enumerable, entry_type = self.Top, **options
+#   
+#   @todo
+#     YARDdoc method name has an `_` suffix added to hack around an apparent
+#     bug in YARD - it seems the method name `Enumerable` matching the 
+#     `Enumerable` class causes failure:
+#     
+#     ```
+#     [error]: Exception occurred while generating 'NRSER/Types/Type.html'
+#     [error]: ArgumentError: Invalid namespace object: #<yardoc method NRSER::Types.Enumerable>:YARD::CodeObjects::MethodObject
+#     [error]: Stack trace:
+#     	//.bundle/ruby/2.3.0/gems/yard-0.9.16/lib/yard/code_objects/proxy.rb:67:in `initialize'
+#     	//.bundle/ruby/2.3.0/gems/yard-0.9.16/lib/yard/registry_resolver.rb:87:in `new'
+#     	//.bundle/ruby/2.3.0/gems/yard-0.9.16/lib/yard/registry_resolver.rb:87:in `lookup_by_path'
+#     	//.bundle/ruby/2.3.0/gems/yard-0.9.16/lib/yard/registry.rb:304:in `resolve'
+#     	//.bundle/ruby/2.3.0/gems/yard-0.9.16/lib/yard/templates/helpers/html_helper.rb:292:in `link_object'
+#     	//.bundle/ruby/2.3.0/gems/yard-link_stdlib-0.1.0/lib/yard/link_stdlib/html_helper.rb:77:in `link_object'
+#     ```
+#   
 #   Types that parameterize {Enumerable} values of a homogeneous type.
 #   
 #   @param [Class] enumerable_type
