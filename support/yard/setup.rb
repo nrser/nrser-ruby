0e2c42286d91884159d295c0fe3b2b9cc4411821
nrser <neil@neilsouza.com>
2019-01-10 00:15:20 +0800

    Re-org .yardopts and add empty setup script file
    Was planning to use the setup script, but haven't gotten
    there yet...

---
+++ support/yard/setup.rb
@@ -0,0 +1,15 @@
+##############################################################################
+# Custom YARD Setup Script
+# ============================================================================
+#
+# This file is loaded when running `yard` via the `--load` CLI option, through a
+# line in `//.yardopts`.
+# 
+# It's here to perform custom configuration by interacting with the YARD API
+# directly in Ruby.
+#
+##############################################################################
+
+# require 'yard'
+
+# YARD::Tags::Library.define_tag "Testees", :test, :with_types
