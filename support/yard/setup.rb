f6a7c69aadd72d2870b3b81c9f876dc1d8f050fa
nrser <neil@neilsouza.com>
2019-01-11 03:01:49 +0800

    Bump yard-cucumber submod to render cuc text
    Includes my mods to render markup in text from Cucumber
    feature files, and turn it on in {YARD::Config} via that
    {file:support/yard/setup.rb} file that I have YARD loading.

--- support/yard/setup.rb
+++ support/yard/setup.rb
@@ -10,6 +10,21 @@
 #
 ##############################################################################
 
-# require 'yard'
+require 'yard'
+
+# Render text from Cucumber features using the same markup (Commonmark) as the
+# rest of things
+YARD::Config.options[ :'yard-cucumber.markup' ] = :default
+
+
+# if ENV['NRSER_PRY']
+#   require 'pry'
+  
+#   # Don't load pryrc - we went the env exactly how it is, and there's a huge mess
+#   # of shit in there
+#   Pry.config.should_load_rc = false
+  
+#   binding.pry
+# end
 
 # YARD::Tags::Library.define_tag "Testees", :test, :with_types
