574454f9715b3ec91f188afd13c2ee5d3e2fb720
nrser <neil@neilsouza.com>
2019-02-10 05:22:00 +0800

    Add new "it has error" step handler for desc/cuc

--- lib/nrser/described/cucumber/steps/attributes.rb
+++ lib/nrser/described/cucumber/steps/attributes.rb
@@ -40,10 +40,16 @@ module Attributes
     end
   
   
-  HAS_ATTRIBUTE = \
-    Step "the {described} has a {method_name} attribute" \
+  THE_X_HAS_ATTRIBUTE = \
+    Step "the {described} has a(n) {method_name} attribute" \
     do |described, method_name|
       describe :attribute, object: described, name: method_name.bare_name
+    end
+    
+  IT_HAS_ATTRIBUTE = \
+    Step "it has a(n) {method_name} attribute" \
+    do |method_name|
+      describe :attribute, name: method_name.bare_name
     end
   
 end # module Attributes
