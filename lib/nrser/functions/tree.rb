4d81439914024486f73b16d55f66d12ad5ac840e
nrser <neil@neilsouza.com>
2019-02-12 17:44:06 +0800

    Get ird of map/transoform tree stuff
    It was half-assed and only half-used in something that's dead now

--- lib/nrser/functions/tree.rb
+++ lib/nrser/functions/tree.rb
@@ -6,7 +6,5 @@
 
 require_relative './tree/each_branch'
 require_relative './tree/map_branches'
-require_relative './tree/map_tree'
 require_relative './tree/leaves'
 require_relative './tree/map_leaves'
-require_relative './tree/transform'
