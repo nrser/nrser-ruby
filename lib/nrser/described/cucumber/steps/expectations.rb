8ec5d6a3ea3bff3db985ecb94e43393ae80176f1
nrser <neil@neilsouza.com>
2019-03-25 07:51:43 +0800

    Add more let binding expectation steps
    TODO Really need to systematize the expectation steps, there's
    getting to really be a ton of them.

--- lib/nrser/described/cucumber/steps/expectations.rb
+++ lib/nrser/described/cucumber/steps/expectations.rb
@@ -34,113 +34,98 @@ module Expectations
   # Steps
   # ==========================================================================
   
-  X_IS_A = \
-    Step "the {described_name} is a(n) {class}" do |described_name, cls|
-      expect_described( described_name ).to be_a cls
-    end
+  Step "the {described_name} is a(n) {class}" do |described_name, cls|
+    expect_described( described_name ).to be_a cls
+  end
   
   
-  X_IS_AN_INSTANCE_OF_THE_Y = \
-    Step "the {described} is an instance of the {described}" \
-    do |described_object, described_class|
-      expect_described( described_object ).
-        to be_a described_class.resolve!( hierarchy ).subject
-      
-      # Need to touch the object again since it's the subject
-      hierarchy.touch described_object
-    end
-  
-  
-  IT_IS_A = \
-    Step "it is a(n) {class}" do |cls|
-      expect_it.to be_a cls
-    end
-  
-  
-  IT_IS_A_SUBCLASS_OF = \
-    Step "it is a subclass of {class}" do |cls|
-      expect_it.to be < cls
-    end
-  
-  
-  IT_IS = \
-    Step "it is {value}" do |value|
-      expect_it.to be value
-    end
-  
-  
-  IT_IS_EQUAL_TO = \
-    Step "it is equal to {value}" do |value|
-      expect_it.to eq value
-    end
+  Step "the {described} is an instance of the {described}" \
+  do |described_object, described_class|
+    expect_described( described_object ).
+      to be_a described_class.resolve!( hierarchy ).subject
     
-    
-  IT_IS_NOT_EQUAL_TO = \
-    Step "it is NOT equal to {value}" do |value|
-      expect_it.not_to eq value
-    end
+    # Need to touch the object again since it's the subject
+    hierarchy.touch described_object
+  end
   
   
-  X_IS_EQUAL_TO_INLINE = \
-    Step "the {described} is equal to {value}" do |described, value|
-      expect( subject ).to eq value
-    end
+  Step "it is a(n) {class}" do |cls|
+    expect_it.to be_a cls
+  end
+  
+  
+  Step "it is a subclass of {class}" do |cls|
+    expect_it.to be < cls
+  end
+  
+  
+  Step "it is {value}" do |value|
+    expect_it.to be value
+  end
+  
+  
+  Step "it is equal to {value}" do |value|
+    expect_it.to eq value
+  end
+  
+  
+  Step "it is NOT equal to {value}" do |value|
+    expect_it.not_to eq value
+  end
+  
+  
+  Step "the {described} is equal to {value}" do |described, value|
+    expect( subject ).to eq value
+  end
 
   
-  X_IS_EQUAL_TO_SRC = \
-    Step "the {described} is equal to:" do |described, string|
-      expect( subject ).to eq eval( string )
-    end
+  Step "the {described} is equal to:" do |described, string|
+    expect( subject ).to eq eval( string )
+  end
     
     
-  X_IS_EQUAL_TO_STRING = \
-    Step "the {described} is equal to the string:" do |described, string|
-      expect( subject ).to eq string
-    end
+  Step "the {described} is equal to the string:" do |described, string|
+    expect( subject ).to eq string
+  end
 
   
-  X_IS_INLINE = \
-    Step "the {described_name} is {value}" \
-    do |described_name, value|
-      expect_described( described_name ).to be value
-    end
+  Step "the {described_name} is {value}" \
+  do |described_name, value|
+    expect_described( described_name ).to be value
+  end
   
   
-  IT_HAS_ATTR_EQUAL_TO = \
-    Step "it has a(n) {method_name} attribute equal to {value}" \
-    do |method_name, value|
-      expect_it.to have_attributes method_name.bare_name => value
-    end
+  Step "it has a(n) {method_name} attribute equal to {value}" \
+  do |method_name, value|
+    expect_it.to have_attributes method_name.bare_name => value
+  end
 
   
-  IT_HAS_ATTR_THAT_IS = \
-    Step "it has a(n) {method_name} attribute that is {value}" \
-    do |method_name, value|
-      expect_it.to have_attributes method_name.bare_name => be( value )
-    end
+  Step "it has a(n) {method_name} attribute that is {value}" \
+  do |method_name, value|
+    expect_it.to have_attributes method_name.bare_name => be( value )
+  end
 
   
-  IT_HAS_ATTR_THAT_IS_A = \
-    Step "it has a(n) {method_name} attribute that is a(n) {class}" \
-    do |method_name, cls|
-      expect_it.to have_attributes \
-        method_name.bare_name => be_a( cls )
-    end
+  Step "it has a(n) {method_name} attribute that is a(n) {class}" \
+  do |method_name, cls|
+    expect_it.to have_attributes \
+      method_name.bare_name => be_a( cls )
+  end
+
   
-  
-  THE_X_HAS_ATTR_THAT_IS_A = \
-    Step "the {described} has a(n) {method_name} attribute that is a(n) {class}" \
-    do |described, method_name, cls|
-      expect( subject ).to have_attributes \
-        method_name.bare_name => be_a( cls )
-    end
+  Step "the {described} has a(n) {method_name} attribute that is a(n) {class}" \
+  do |described, method_name, cls|
+    expect( subject ).to have_attributes \
+      method_name.bare_name => be_a( cls )
+  end
     
-  THE_DESCRIBED_HAS_ATTRIBUTE_THAT_IS_A = \
-    Step "the {described} has a(n) {method_name} attribute that is {value}" \
-    do |described, method_name, value|
-      expect_described( described ).to have_attributes \
-        method_name.bare_name => be( value )
-    end
+  
+  Step "the {described} has a(n) {method_name} attribute that is {value}" \
+  do |described, method_name, value|
+    expect_described( described ).to have_attributes \
+      method_name.bare_name => be( value )
+  end
     
   
   Step "the {described} has a {value} key with value {value}" \
@@ -162,6 +147,11 @@ module Expectations
   
   Step "{local_var_name} is equal to the string:" do |local_var_name, string|
     expect( resolve_let local_var_name ).to eq string
+  end
+  
+  
+  Step "{local_var_name} is a(n) {class}" do |local_var_name, cls|
+    expect( resolve_let local_var_name, describe: true ).to be_a cls
   end
 
 end # module Expectations
