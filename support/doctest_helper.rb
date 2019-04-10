7db28206a1777dc2e5d67ebe58f295ec241d31e5
nrser <neil@neilsouza.com>
2019-04-10 12:59:04 +0800

    Hack doctest dynamic scoping
    Read the comment, it's funky shit.
    
    Monkey-patched 'cause not sure exactly what I want
    to do about it yet *and* fucking Trav is having trouble
    using the `yard-doctest` dev gem.

--- support/doctest_helper.rb
+++ support/doctest_helper.rb
@@ -20,6 +20,51 @@ rescue
   log.warn "Unable to require from filepath #{ filepath.inspect }"
 end
 
+
+class ::YARD::Doctest::Example < ::Minitest::Spec
+  def context(bind)
+    @context ||= begin
+      if bind
+        # NRSER - This is... funky. If we use a regular `#class_eval`, 
+        # it's possible for name resolution to work up from *here* first,
+        # which is problematic if you happen to been looking for something
+        # like {NRSER::Text} *but* there is also an {YARD::NRSER} module
+        # loaded, because it will go for {YARD::NRSER::Text}, and fail.
+        # 
+        # This seems like a ridiculously edge case I've gotten myself into,
+        # but I also can't come to any reasons why I *would* be wanting to
+        # resolve out of {YARD::Doctest::Example}, and this change makes
+        # things a lot nicer and more natural for the {NRSER} gem.
+        # 
+        # This what was here:
+        # 
+        #     context = bind.class_eval('binding', __FILE__, __LINE__)
+        # 
+        # and this is the replacement. By reaching up into the top-level 
+        # binding to do our dirty work, we get a binding that resolves 
+        # much more like I would expect.
+        # 
+        # TODO  Could maybe use some clean up work..?
+        # 
+        context = TOPLEVEL_BINDING.
+          eval "#{ bind.name }.class_eval('binding', __FILE__, __LINE__)"
+        
+        # Oh my god, what is happening here?
+        # We need to transplant instance variables from the current binding.
+        instance_variables.each do |instance_variable_name|
+          local_variable_name = "__yard_doctest__#{instance_variable_name.to_s.delete('@')}"
+          context.local_variable_set(local_variable_name, instance_variable_get(instance_variable_name))
+          context.eval("#{instance_variable_name} = #{local_variable_name}")
+        end
+        context
+      else
+        binding
+      end
+    end
+  end
+end
+
+
 YARD::Doctest.configure do |doctest|
   doctest.before do |example|
     # this is called before each example and
